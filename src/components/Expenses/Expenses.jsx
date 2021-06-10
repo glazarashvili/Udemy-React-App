a22735e763450b688eb0ddc589928fa04097af0c
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-06-10 11:04:50 +0400

    added expenses list

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,15 +2,13 @@ import React, { useState } from 'react'
 
 import './Expenses.css'
 import Card from '../UI/Card'
-import ExpenseItem from './ExpenseItem'
+import ExpensesList from './ExpensesList'
 import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = ({ items }) => {
   const [filteredYear, setFilteredYear] = useState('2020')
 
   const filterChangeHandler = (selectedYear) => {
-    console.log('exp .js')
-    console.log(selectedYear)
     setFilteredYear(selectedYear)
   }
 
@@ -24,14 +22,7 @@ const Expenses = ({ items }) => {
         selected={filteredYear} 
         onChangeFilter={filterChangeHandler} 
       />
-      {filteredExpenses.map(item => {
-        return <ExpenseItem 
-          key={item.id} 
-          title={item.title} 
-          amount={item.amount} 
-          date={item.date} 
-          />
-      })}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   )
 }
